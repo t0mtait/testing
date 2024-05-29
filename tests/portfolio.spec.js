8dc04e0b74b94e382505fb6d504b4f8cc253c08b
tom tait <thomas.t.ca@outlook.com>
2024-05-29 13:13:43 -0300

    initial commit

---
+++ tests/portfolio.spec.js
@@ -0,0 +1,46 @@
+const { test, expect } = require('@playwright/test');
+
+test('element visibility testing', async ({ page }) => {
+  await page.goto('https://www.tomtait.dev');
+  await expect(page).toHaveURL('https://www.tomtait.dev');
+  await expect(page).toHaveTitle(/Tom Tait Portfolio/);
+  await expect(page.locator('.nav')).toBeVisible();
+  await expect(page.locator('#aboutsection')).toBeVisible();
+  await expect(page.locator('#experiencesection')).toBeVisible();
+  await expect(page.locator('#projectssection')).toBeVisible();
+  await expect(page.locator('#contactsection')).toBeVisible();
+});
+
+test('button redirect testing', async ({ page }) => {
+  await page.goto('https://www.tomtait.dev');
+
+  await page.locator('#btnSwitch').click();
+  await expect(page.locator('#btnSwitch')).toHaveText('Light');
+
+  await page.locator('#btnSwitch').click();
+  await expect(page.locator('#btnSwitch')).toHaveText('Dark');
+
+  await page.locator('#linkedin').click();
+  await expect(page).toHaveURL('https://www.linkedin.com/in/tom-tait/');
+  await page.goto('https://www.tomtait.dev');
+
+  await page.locator('#github').click();
+  await expect(page).toHaveURL('https://github.com/t0mtait');
+  await page.goto('https://www.tomtait.dev');
+
+  await page.locator('#simonView').click();
+  await expect(page).toHaveURL('https://simon.tomtait.dev');
+  await page.goto('https://www.tomtait.dev');
+
+  await page.locator('#simonGit').click();
+  await expect(page).toHaveURL('https://github.com/t0mtait/simon');
+  await page.goto('https://www.tomtait.dev');
+
+  await page.locator('#translatorView').click();
+  await expect(page).toHaveURL('https://translator.tomtait.dev');
+  await page.goto('https://www.tomtait.dev');
+
+  await page.locator('#translatorGit').click();
+  await expect(page).toHaveURL('https://github.com/t0mtait/translator');
+  await page.goto('https://www.tomtait.dev');
+});
